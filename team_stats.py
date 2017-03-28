daea5a5156ce7522527b3522f33e12161714f590
Paul Deng <paulliwali@hotmail.com>
2017-03-27 21:25:58 -0400

    fixed mistakes in team_stats
    the homeTeamDiff array was storing all the difference and not
    re-initializing at the beginning of the for loop

--- team_stats.py
+++ team_stats.py
@@ -12,34 +12,51 @@ gameDf = pd.read_csv('games.csv', sep='\t', index_col=0)
 teamMiscDf = pd.read_csv('team_misc.csv', index_col=0)
 teamDf = pd.read_csv('team.csv', index_col=0)
 opponetDf = pd.read_csv('team_opponent.csv', index_col=0)
-homeTeamDiff = np.array([])
+
 
 # Add a new blank column to store the shooting % difference
 teamMiscDf['avgDiff'] = pd.Series(np.zeros(31))
 
-## Example of 1 team
-#homeTeamName = "CHI"
-#Df = gameDf.loc[gameDf['Home Team'] == homeTeamName]
+# Adjust the FG% as if the home team is an team with average DRtg
+# with the league averge DRtg of 104.6
+# The regression between DRtg and FG% is:
+# FG% = 0.004*DRtg + 0.0281
+def adjustFG(DRtg, FG):
+    leagueAvgDRtg = 104.6
+    adjFG = (0.004*(DRtg) + 0.0281) - (0.004*(leagueAvgDRtg) + 0.0281)
+    return (FG-adjFG)
 
 for homeTeamName in gameDf['Home Team'].unique():
+    homeTeamDiff = np.array([])
     Df = gameDf.loc[gameDf['Home Team'] == homeTeamName]
+    
     for awayTeamName in Df['Away Team'].unique():
         Df2 = Df.loc[Df['Away Team'] == awayTeamName]
         # This stores the average shooting percentage the away team
         # shoots at the home team stadium
         awayFG = Df2['Away Shooting Percentage'].mean()
         
+        # Adjust the FG% of away team against the defensive abilities
+        # of the home team as if it is playing a league average team
+        homeTeamStats = teamMiscDf.loc[teamMiscDf['Name'] == homeTeamName]
+        DRtg = float(homeTeamStats['DRtg'])
+        adjawayFG = adjustFG(DRtg, awayFG)
+        
         awayTeamStats = teamDf.loc[teamDf['Name'] == awayTeamName]
         # This stores the average shooting percentage of the away team
         awayAverageFG = float(awayTeamStats['FG%'])
         
-        # This stores the difference between average shooting percentage
+        # This stores the difference between adjusted average shooting percentage
         # and the shooting percentage at the stadium 
-        diff = awayAverageFG - awayFG
+        # +ve means the team shoots better than average
+        # -ve means the team shoots worse than average
+        diff = adjawayFG - awayAverageFG
         homeTeamDiff = np.append(homeTeamDiff, diff)
-        avgDiff = np.mean(homeTeamDiff)
+    
+    print len(homeTeamDiff)
+    avgDiff = np.mean(homeTeamDiff)
         
-        # Add the avgDiff into the appropriate place in the teamMiscDf
-        teamMiscDf.loc[teamMiscDf['Name'] == homeTeamName, 'avgDiff'] = avgDiff
+    # Add the avgDiff into the appropriate place in the teamMiscDf
+    teamMiscDf.loc[teamMiscDf['Name'] == homeTeamName, 'avgDiff'] = avgDiff
                       
 teamMiscDf.to_csv("analysis.csv", sep=',')                      
