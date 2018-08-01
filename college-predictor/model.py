5f19bd308125075c243218ac057958e1f99c141e
paulliwali <paulliwali@hotmail.com>
2018-08-01 15:46:40 -0400

    recording histogram on tensorboard

--- college-predictor/model.py
+++ college-predictor/model.py
@@ -8,25 +8,23 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.callbacks import CSVLogger, TensorBoard
 
-def runNeuralNetwork(dataName, epochs, batch, learningRate, exportCSV):
+def runNeuralNetwork(dataName, epochs, batch, learningRate):
     # Fix the random seed for testing
     np.random.seed(7)
 
     dir = os.path.dirname(__file__)
 
-    # Export the results into a folder with appropriate
-    if exportCSV:
-        csvFilename = '%s_e%s_b%s_lr%s.csv' % (dataName, epochs, batch, learningRate)
-        csvPathname = os.path.join(dir, 'results', csvFilename)
-
     # Reading data
     filename = os.path.join(dir, 'data', 'working', dataName)
     dataset = pd.read_csv(filename, comment='#')
-    # TODO Only the first 18 variable is looked at, need a way to identify which subset of
-    # variables to include
     X = dataset.iloc[:,4:22]
     Y = dataset.iloc[:,3]
-    print(X)
+
+    # Reading test data
+    filename_test = os.path.join(dir, 'data', 'working', '2010-2017-pace-working.csv')
+    dataset_test = pd.read_csv(filename_test, comment='#')
+    X_test = dataset_test.iloc[:,4:22]
+    Y_test = dataset_test.iloc[:,3]
 
     # Rescale the X to between 0 and 1
     # Result - worse than normal setup
@@ -46,36 +44,23 @@ def runNeuralNetwork(dataName, epochs, batch, learningRate, exportCSV):
     model.add(Dense(1, activation='sigmoid'))
 
     # Compile the model
-    # Initialize two different optimizers
+    # Initialize two different optimizers, using adam at the moment
     adam = optimizers.adam(lr=learningRate)
     sgd = optimizers.sgd(lr=learningRate, momentum=0.01)
     model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
 
-    # Fit the model
-    csv_logger = CSVLogger(csvPathname, separator=',', append=False)
-    tensorboard = TensorBoard(log_dir=os.path.join(dir, "results"), histogram_freq=0, write_graph=True,
-                                write_images=False)
-    model.fit(X.values, Y.values, epochs=epochs, batch_size=batch, callbacks=[csv_logger, tensorboard])
+    # Create tensorboard object
+    tensorboard = TensorBoard(log_dir=os.path.join(dir, "results"), histogram_freq=10, write_graph=True,
+                              write_images=False)
 
-    # Make predictions with the model
-    predictions = model.predict(X.values)
-    rounded = [round(x[0]) for x in predictions]
-    print(rounded)
-    # With L0 - 8, L1 - 2 it produces okay results of accuracy of 0.8108
-
-    # Evaluate the model with test data
-    filename_test = os.path.join(dir, 'data', 'working', '2010-2017-pace-working.csv')
-    dataset_test = pd.read_csv(filename_test, comment='#')
-    X_test = dataset_test.iloc[:,4:22]
-    Y_test = dataset_test.iloc[:,3]
-    scores = model.evaluate(X_test.values, Y_test.values)
-    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    # Fit the model and record to tensorboard
+    model.fit(X.values, Y.values, epochs=epochs, batch_size=batch, validation_data=(X_test, Y_test), callbacks=[tensorboard])
 
 if __name__ == "__main__":
     dataName = "2000-2009-pace-working.csv"
     epochs = 500
     batch = 10
-    learningRate = 0.001
+    learningRate = 0.005
     exportCSV = True
 
-    runNeuralNetwork(dataName, epochs, batch, learningRate, exportCSV)
+    runNeuralNetwork(dataName, epochs, batch, learningRate)
