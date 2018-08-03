407a01d85d25aa56218fed29c51d85a3b064512e
paulliwali <paulliwali@hotmail.com>
2018-08-03 11:34:57 -0400

    new scan with proper settings on the dropout. Manual end due to time.

--- college-predictor/model.py
+++ college-predictor/model.py
@@ -18,10 +18,10 @@ def readInputs(inputDataName):
     # Reading data
     filename = os.path.join(dir, 'data', 'working', inputDataName)
     dataset = pd.read_csv(filename, comment='#')
-    X = dataset.iloc[:,4:22]
-    Y = dataset.iloc[:,3]
+    x = dataset.iloc[:,4:22]
+    y = dataset.iloc[:,3]
 
-    return X, Y
+    return x, y
 
 def scan_hyperparameter(x_train, y_train, x_val, y_val, params):
     '''
@@ -33,7 +33,6 @@ def scan_hyperparameter(x_train, y_train, x_val, y_val, params):
     model.add(Dense(params['first_neuron'], input_dim=x_train.shape[1],
                     activation=params['activation'],
                     kernel_initializer=params['kernel_initializer']))
-    
     model.add(Dropout(params['dropout']))
     hidden_layers(model, params, 1)
     model.add(Dense(1, activation=params['last_activation'],
@@ -42,10 +41,6 @@ def scan_hyperparameter(x_train, y_train, x_val, y_val, params):
     model.compile(loss=params['losses'],
                   optimizer=params['optimizer'](),
                   metrics=['acc'])
-
-    # # Create tensorboard object
-    # tensorboard = TensorBoard(log_dir=os.path.join(dir, "results"), histogram_freq=10, write_graph=True,
-    #                          write_images=False)
 
     # Fit the model and record to tensorboard
     history = model.fit(x_train, y_train,
@@ -56,17 +51,47 @@ def scan_hyperparameter(x_train, y_train, x_val, y_val, params):
 
     return history, model
 
+def runNeuralNetwork(x, y, epochs, batch, learningRate):
+    dir = os.path.dirname(__file__)
+
+    # Building the model
+    model = Sequential()
+    model.add(Dense(12,
+                    kernel_initializer='random_normal',
+                    input_dim=18,
+                    activation='relu'))
+    model.add(Dropout(0))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(4, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+
+    # Compile the model
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=learningRate), metrics=['accuracy'])
+
+    # Fit the model
+    tensorboard = TensorBoard(log_dir=os.path.join(dir, "results"), histogram_freq=10, write_graph=True,
+                                write_images=False)
+    model.fit(x.values, y.values,
+              validation_split=0.2,
+              epochs=epochs,
+              batch_size=batch,
+              callbacks=[tensorboard])
+
+    # # Make predictions with the model
+    # predictions = model.predict(X.values)
+    # rounded = [round(x[0]) for x in predictions]
+    # print(rounded)
 
 def main():
     inputDataName = "2000-2017-pace.csv"
     x, y = readInputs(inputDataName)
 
-    p = {'lr': (0.001, 1, 10),
+    p = {'lr': (0.001, 0.005, 2),
      'first_neuron':[12, 6],
-     'hidden_layers':(0, 10, 10),
-     'batch_size': [5, 10, 20, 50],
+     'hidden_layers':(0, 10, 4),
+     'batch_size': [10, 20],
      'epochs': [100, 200],
-     'dropout': [0, 2, 4, 8],
+     'dropout': (0, 1, 3),
      'kernel_initializer': ['uniform', 'normal'],
      'optimizer': [Adam, SGD],
      'losses': [binary_crossentropy, mean_squared_error],
@@ -78,7 +103,12 @@ def main():
     #         model=scan_hyperparameter, 
     #         grid_downsample=1,
     #         params=p, 
-    #         dataset_name='nba', 
-    #         experiment_no='k')
+    #         dataset_name='college_stats', 
+    #         experiment_no='2')
+
+    epochs = 200
+    batch = 10
+    learningRate = 0.001
+    runNeuralNetwork(x, y, epochs, batch, learningRate)
 
 if __name__ == "__main__": main()
